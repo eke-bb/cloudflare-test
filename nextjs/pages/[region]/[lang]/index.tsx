d5da979ccccfe45c17a6ca8a29fffc5febe1f6d8
Eric Ellingson <eellingson@bluebottlecoffee.com>
2023-04-14 13:44:19 -0700

    regions

--- nextjs/pages/[region]/[lang]/index.tsx
+++ nextjs/pages/[region]/[lang]/index.tsx
@@ -6,16 +6,21 @@ const inter = Inter({ subsets: ['latin'] })
 
 
 export async function getStaticPaths() {
-  return {
-    // paths: [{ params: { id: '1' } }, { params: { id: '2' } }],
-    paths: localization.languages("us").map(language => {
-      return {
+  const paths: any[] = [];
+
+  Object.keys(localization.regions).forEach(region => {
+    localization.languages(region).forEach(language => {
+      paths.push({
         params: {
-          region: "us",
+          region: region,
           lang: language.code,
         }
-      }
-    }),
+      })
+    })
+  });
+
+  return {
+    paths,
     fallback: false, // can also be true or 'blocking'
   }
 }
@@ -31,9 +36,20 @@ export async function getStaticProps(context: any) {
 }
 
 export default function Home({region, lang}: {region: string, lang: string}) {
+  const paths: any[] = ["/"];
+
+  Object.keys(localization.regions).forEach(region => {
+    localization.languages(region).forEach(language => {
+      paths.push(`/${region}/${language.code}`)
+    })
+  });
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
+
+        {paths.map(path => <a style={{textDecoration: "underline"}} href={path}>{path}</a>)}
+
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           Get started by editing&nbsp;
           <code className="font-mono font-bold">pages/index.tsx [{region}/{lang}]</code>
