ff26ca9ad3b0d8fc4ff6499a1e3b1905fc6521b8
Eric Ellingson <eellingson@bluebottlecoffee.com>
2023-04-14 13:00:59 -0700

    add nextjs test app

---
+++ nextjs/next.config.js
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  experimental: {
+    // see https://github.com/vercel/next.js/pull/22867
+    // this is required to be able to include the `<root>/localization.ts` file,
+    // which is outside of the `site/nextjs` directory
+    externalDir: true,
+  },
+}
+
+module.exports = nextConfig
